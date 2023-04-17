6e4dabac7100de245abc58947cf7934a3bf57ceb
thomas <thomasherondale@gmail.com>
2023-04-17 17:05:20 +0200

    AddToCartContract da testare

--- app/src/main/java/com/projects/foodace/CartFragment.kt
+++ app/src/main/java/com/projects/foodace/CartFragment.kt
@@ -1,34 +1,37 @@
 package com.projects.foodace
 
+import android.app.Activity.RESULT_OK
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.navigation.fragment.NavHostFragment
+import com.projects.foodace.model.Food
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CartFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CartFragment : NavHostFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cart, container, false)
     }
+}
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         * @return A new instance of fragment [CartFragment].
-         */
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) = CartFragment()
+class AddToCartContract : ActivityResultContract<Food, Int>() {
+    override fun createIntent(context: Context, input: Food) =
+        Intent(context, FoodDetailsActivity::class.java).apply {
+            putExtra("food", input)
+        }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): Int {
+        return if (resultCode == RESULT_OK)
+            intent!!.extras!!.getInt("quantity")
+        else
+            throw IllegalStateException("ShowDetailsActivity did not provide quantity.")
     }
+
 }

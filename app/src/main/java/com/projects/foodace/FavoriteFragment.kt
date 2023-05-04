f8a9b8de7290086b56fb299f67291889f5ac34fe
thomas <thomasherondale@gmail.com>
2023-05-04 18:12:20 +0200

    fragment

--- app/src/main/java/com/projects/foodace/FavoriteFragment.kt
+++ app/src/main/java/com/projects/foodace/FavoriteFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.projects.foodace.databinding.FragmentFavoriteBinding
+import com.projects.foodace.model.FavoriteFoodsViewModel
 
 
 class FavoriteFragment : Fragment() {
+    private lateinit var binding: FragmentFavoriteBinding
+    private val viewModel: FavoriteFoodsViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+    ): View {
+        binding = FragmentFavoriteBinding.inflate(layoutInflater)
+        return binding.root
     }
 }

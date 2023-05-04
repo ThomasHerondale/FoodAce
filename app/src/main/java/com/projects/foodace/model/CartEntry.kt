4a416090895a01a3abe4039f69ec8742b86e545d
thomas <thomasherondale@gmail.com>
2023-05-04 11:36:39 +0200

    toString più sintetico per CartEntry

--- app/src/main/java/com/projects/foodace/model/CartEntry.kt
+++ app/src/main/java/com/projects/foodace/model/CartEntry.kt
@@ -1,3 +1,7 @@
 package com.projects.foodace.model
 
-data class CartEntry(val food: Food, val quantity: Int)
+data class CartEntry(val food: Food, val quantity: Int) {
+    override fun toString(): String {
+        return "(${food.name}, $quantity)"
+    }
+}

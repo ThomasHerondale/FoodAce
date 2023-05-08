5f4312b4f264d0e21ac756d0644dd736cf5562da
thomas <thomasherondale@gmail.com>
2023-05-08 11:09:03 +0200

    scherzavo, niente safe access

--- app/src/main/java/com/projects/foodace/LoggedApplication.kt
+++ app/src/main/java/com/projects/foodace/LoggedApplication.kt
@@ -6,7 +6,6 @@ import com.projects.foodace.database.User
 class LoggedApplication(
 ) : Application() {
     var username: String? = null
-        get() = field ?: throw IllegalStateException("Login has not been registered correctly.")
         set(value) =
             if (username != null)
                 throw IllegalStateException("Login is already registered.")

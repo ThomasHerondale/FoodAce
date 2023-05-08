02a528f9a0d5fb041b22a493fe36b04653c23b38
thomas <thomasherondale@gmail.com>
2023-05-08 10:50:04 +0200

    safe access a username in LoggedApplication

--- app/src/main/java/com/projects/foodace/LoggedApplication.kt
+++ app/src/main/java/com/projects/foodace/LoggedApplication.kt
@@ -6,6 +6,7 @@ import com.projects.foodace.database.User
 class LoggedApplication(
 ) : Application() {
     var username: String? = null
+        get() = field ?: throw IllegalStateException("Login has not been registered correctly.")
         set(value) =
             if (username != null)
                 throw IllegalStateException("Login is already registered.")

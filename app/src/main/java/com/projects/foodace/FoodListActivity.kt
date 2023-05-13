7b65c0b26d1da6249bccaedde34828f4b12bdc58
thomas <thomasherondale@gmail.com>
2023-05-13 16:56:30 +0200

    filtro sulle chip per categorie

--- app/src/main/java/com/projects/foodace/FoodListActivity.kt
+++ app/src/main/java/com/projects/foodace/FoodListActivity.kt
@@ -3,11 +3,15 @@ package com.projects.foodace
 import android.app.SearchManager
 import android.content.Intent.ACTION_SEARCH
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.SearchView.OnQueryTextListener
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
+import com.google.android.material.chip.Chip
 import com.projects.foodace.databinding.ActivityFoodListBinding
 import com.projects.foodace.model.CartViewModel
 import com.projects.foodace.model.Category
@@ -20,6 +24,9 @@ class FoodListActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFoodListBinding
     private val foodListViewModel: FoodListViewModel by viewModels()
     private val cartViewModel: CartViewModel by lazy { applicationViewModels() }
+
+    // ChipGroup handling
+    private val selections = Array( 5) { false }
 
     private val detailsActivityLauncher = registerForActivityResult(AddToCartContract()) {
         if (it.quantity != 0)
@@ -59,6 +66,7 @@ class FoodListActivity : AppCompatActivity() {
 
         binding.backButton.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
         initFoodList()
+        initChipGroup()
     }
 
     private fun initFoodList() {
@@ -68,5 +76,28 @@ class FoodListActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(context, VERTICAL, false)
         }
         foodListViewModel.foods.observe(this) { adapter.submitList(it) }
+    }
+
+    private fun initChipGroup() {
+        binding.categoriesChips.children.forEach { view: View ->
+            val chip = view as Chip
+            chip.setOnCheckedChangeListener { _, _ ->
+                binding.categoriesChips.apply {
+                    val checkedIdxs = checkedChipIds.map { getChipIndex(it) }
+                    for (idx in selections.indices)
+                        selections[idx] = idx in checkedIdxs
+                    Log.v("FODD_LIST", "Filtering for ${selections.contentToString()}")
+                }
+            }
+        }
+    }
+
+    private fun getChipIndex(id: Int): Int = when (id) {
+        R.id.burgerChip -> 0
+        R.id.pizzaChip -> 1
+        R.id.hotdogChip -> 2
+        R.id.drinkChip -> 3
+        R.id.dessertChip -> 4
+        else -> throw IllegalStateException("Unknown chip id.")
     }
 }

25115edce610bb7b03373a7c2e4a00d15b04df2c
thomas <thomasherondale@gmail.com>
2023-05-13 18:00:12 +0200

    la categoria all'avvio è selezionata automaticamente; svuotare la barra di ricerca annulla il filtro per nome

--- app/src/main/java/com/projects/foodace/FoodListActivity.kt
+++ app/src/main/java/com/projects/foodace/FoodListActivity.kt
@@ -9,6 +9,7 @@ import android.widget.SearchView.OnQueryTextListener
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import androidx.core.view.get
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
 import com.google.android.material.chip.Chip
@@ -40,10 +41,14 @@ class FoodListActivity : AppCompatActivity() {
 
         when (intent.action) {
             ACTION_SEARCH ->
-                intent.getStringExtra(SearchManager.QUERY)?.also { foodListViewModel.getFoods(it) }
+                intent.getStringExtra(SearchManager.QUERY)?.also {
+                    foodListViewModel.getFoods(it)
+                    binding.searchBar.setQuery(it, false)
+                }
             FILTER ->
                 intent.extras?.get(CATEGORY_KEY)?.also {
                     foodListViewModel.getFoods(categories = listOf(it as Category))
+                    checkCategory(it)
                 }
             else -> TODO()
         }
@@ -52,15 +57,19 @@ class FoodListActivity : AppCompatActivity() {
             setOnQueryTextListener(object : OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     if (query != null) {
-                        foodListViewModel.getFoods(query)
+                        foodListViewModel.getFoods(query, selectedCategories())
                     }
                     return true
                 }
 
                 override fun onQueryTextChange(newText: String?): Boolean {
-                    return false
+                    // Handle the x button in the search bar
+                    if (newText?.isEmpty() == true)
+                        foodListViewModel.getFoods(categories = selectedCategories())
+                    return true
                 }
             })
+            // Don't show keyboard on start
             clearFocus()
         }
 
@@ -88,16 +97,32 @@ class FoodListActivity : AppCompatActivity() {
                         selections[idx] = idx in checkedIdxs
                     Log.v("FODD_LIST", "Filtering for ${selections.contentToString()}")
                 }
+                val query = binding.searchBar.query.toString()
+                foodListViewModel.getFoods(query, selectedCategories())
             }
         }
     }
 
-    private fun getChipIndex(id: Int): Int = when (id) {
+    private fun getChipIndex(id: Int) = when (id) {
         R.id.burgerChip -> 0
         R.id.pizzaChip -> 1
         R.id.hotdogChip -> 2
         R.id.drinkChip -> 3
         R.id.dessertChip -> 4
         else -> throw IllegalStateException("Unknown chip id.")
+    }
+
+    private fun selectedCategories(): List<Category>? {
+        val list = mutableListOf<Category>()
+        Category.values().forEachIndexed { index, category ->
+            if (selections[index])
+                list.add(category)
+        }
+        return list.ifEmpty { null }
+    }
+
+    private fun checkCategory(category: Category) {
+        val idx = Category.values().indexOf(category)
+        (binding.categoriesChips[idx] as Chip).isChecked = true
     }
 }
